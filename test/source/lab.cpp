50c033bd85ac78b0c34c6dab4ce261e695e969a4
Maxim Mikhaylov <mmikhaylov56@gmail.com>
2021-02-27 17:03:46 +0300

    Changed max delta multiplier to 1

--- test/source/lab.cpp
+++ test/source/lab.cpp
@@ -14,12 +14,12 @@ TEST_CASE("Lab version") {
 
 TEST_CASE("Dichotomy") {
     for (double epsilon = 1e-1; epsilon > 1e-10; epsilon /= 10) {
-        for (double delta_multiplier = 2; delta_multiplier >= 0.1; delta_multiplier -= 0.1) {
+        for (double delta_multiplier = 1; delta_multiplier >= 0.1; delta_multiplier -= 0.1) {
             auto optimizer = lab::Dichotomy(epsilon * delta_multiplier);
             CHECK(abs(optimizer.optimize([](double x) { return x * x; }, epsilon, -1, 1) - 0)
                   <= epsilon);
             CHECK(
-                abs(optimizer.optimize([](double x) { return sin(x); }, epsilon, M_PI / 2, 2 * M_PI)
+                abs(optimizer.optimize(sin, epsilon, M_PI / 2, 2 * M_PI)
                     - M_PI * 3 / 2)
                 <= epsilon);
         }
