b8201bb553b89bea980b7b2a5905efcb20ef48c7
yaroslavprofi <yaroslavprofi@yandex.ru>
2021-02-28 02:38:03 +0300

    added bin

--- gui/source/main.cpp
+++ gui/source/main.cpp
@@ -1,1 +1,17 @@
-int main() {}
+#include <QApplication>
+#include <QWidget>
+#include <QIcon>
+
+int main(int argc, char *argv[]) {
+
+    QApplication app(argc, argv);
+
+    QWidget window;
+
+    window.resize(500, 500);
+    window.setWindowTitle("SOSI");
+    window.setWindowIcon(QIcon("bin.png"));
+    window.show();
+
+    return app.exec();
+}
