ea5f19fb7500805e3ecfe326b8398c191251aa93
Maxim Mikhaylov <mmikhaylov56@gmail.com>
2021-02-28 23:45:19 +0300

    Switched formatting to 80 symbols per line because less doesn't fit into the report

--- source/dichotomy.cpp
+++ source/dichotomy.cpp
@@ -29,14 +29,21 @@ double Dichotomy::answer(Segment current_segment) {
     return (current_segment.end + current_segment.start) / 2;
 }
 
-Segment Dichotomy::new_segment(Segment current_segment, func optimized_function) {
+Segment Dichotomy::new_segment(Segment current_segment,
+                               func optimized_function) {
     double start = current_segment.start;
     double end = current_segment.end;
     double x1 = get_x1(start, end);
     double x2 = get_x2(start, end);
-    return Segment(start, end, {{x1, optimized_function(x1)}, {x2, optimized_function(x2)}});
+    return Segment(
+        start, end,
+        {{x1, optimized_function(x1)}, {x2, optimized_function(x2)}});
 }
 
-double Dichotomy::get_x1(double start, double end) { return (end + start - delta) / 2; }
+double Dichotomy::get_x1(double start, double end) {
+    return (end + start - delta) / 2;
+}
 
-double Dichotomy::get_x2(double start, double end) { return (end + start + delta) / 2; }
+double Dichotomy::get_x2(double start, double end) {
+    return (end + start + delta) / 2;
+}
