44a783910712e81b510244e5d9a96344acc5926c
yaroslavprofi <yaroslavprofi@yandex.ru>
2021-03-02 03:15:11 +0300

    asdasdadasd

--- gui/source/main.cpp
+++ gui/source/main.cpp
@@ -1,7 +1,6 @@
 #include <QPushButton>
 #include <QComboBox>
 #include <QSpinBox>
-#include <QFrame>
 #include <QApplication>
 #include <QWidget>
 #include <QIcon>
@@ -15,39 +14,100 @@
 #include <QSizePolicy>
 #include <QSlider>
 #include <QLabel>
+#include <QVector>
+#include <QPen>
+#include "qcustomplot.h"
+#include "lab/dichotomy.h"
+
+class Drawer : public QWidget {
+public:
+    Drawer(QWidget *parent = 0) {
+        plot_setup();
+        auto optimizer = lab::Dichotomy(1e-5);
+        optimizer.optimize(f, 1e-4, -2, 3);
+        segments = optimizer.get_segments();
+    }
+
+    void draw(int iteration) {
+        auto[a, b, points] = segments[iteration];
+        std::vector<double> x, y, x_f, y_f;
+        points_to_x_y(points, x, y);
+        points_to_x_y(get_f_points(a, b), x_f, y_f);
+        plot->addGraph();
+        plot->graph(0)->setPen(QPen(Qt::blue));
+        plot->graph(0)->setData(QVector<double>::fromStdVector(x_f), QVector<double>::fromStdVector(y_f));
+        plot->addGraph();
+        plot->graph(1)->setPen(QPen(Qt::red));
+        plot->graph(1)->setData(QVector<double>::fromStdVector(x), QVector<double>::fromStdVector(y));
+        plot->graph(1)->rescaleAxes();
+    }
+
+private:
+    QCustomPlot *plot;
+
+    void plot_setup(){
+        plot = new QCustomPlot(this);
+        auto grid = new QGridLayout(this);
+        grid->addWidget(plot);
+        plot->setInteraction(QCP::iRangeDrag, true);
+        plot->setInteraction(QCP::iRangeZoom, true);
+    }
+
+    static double f(double x) {
+        return x * x;
+    }
+
+    std::vector <std::pair<double, double>> get_f_points(double a, double b, int count = 30) {
+        std::vector <std::pair<double, double>> points;
+        double step = (b - a) / count;
+        for (double x = a; x < b; x += step) {
+            points.emplace_back(x, f(x));
+        }
+        return points;
+    }
+
+    void points_to_x_y(std::vector <std::pair<double, double>> points, std::vector<double> &x, std::vector<double> &y) {
+        for (auto point:points) {
+            x.push_back(point.first);
+            y.push_back(point.second);
+        }
+    }
+
+    std::vector <lab::Segment> segments;
+};
 
 class MainWindow : public QWidget {
 public:
     MainWindow(QWidget *parent = 0);
 };
 
+double f(double x) { return x * x; }
+
 MainWindow::MainWindow(QWidget *parent) : QWidget(parent) {
 
-    auto main_h_box = new QHBoxLayout(this);
-    auto left_part = new QFrame(this);
-    auto right_part = new QFrame(this);
-    left_part->setMinimumWidth(200);
-    main_h_box->addWidget(left_part, 1);
-    main_h_box->addWidget(right_part);
+    auto v_box = new QVBoxLayout(this);
+    auto h_box = new QHBoxLayout(this);
 
-    auto left_part_v_box = new QVBoxLayout(left_part);
-    auto graphic = new QFrame(left_part);
-    graphic->setObjectName("graphicFrame");
-    left_part_v_box->addWidget(graphic, 1);
-    auto left_part_h_box = new QHBoxLayout(left_part);
-    auto slider = new QSlider(Qt::Horizontal, left_part);
+    auto graphic = new Drawer(this);
+    auto combo_box = new QComboBox(this);
+    auto slider = new QSlider(Qt::Horizontal, this);
+    auto label = new QLabel("0", this);
+
+    v_box->addWidget(graphic, 1);
+    v_box->addLayout(h_box);
+    h_box->addWidget(combo_box);
+    h_box->addWidget(slider);
+    h_box->addWidget(label);
+
+    graphic->draw(10);
+    graphic->setStyleSheet("background: yellow");
+    QStringList optimizators = {"Дихотомия", "Золотое сечение", "Фиббоначи", "Параболы", "Брент"};
+    combo_box->addItems(optimizators);
+    combo_box->setFixedWidth(200);
     slider->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-    left_part_h_box->addWidget(slider);
-    auto label = new QLabel("0", left_part);
-    left_part_h_box->addWidget(label);
-    left_part_v_box->addLayout(left_part_h_box);
+    connect(slider, &QSlider::valueChanged, label,
+            static_cast<void (QLabel::*)(int)>(&QLabel::setNum));
 
-    QStringList optimizators = {"hui", "mocha", "govno"};
-    auto right_combo_box = new QComboBox(right_part);
-    right_combo_box->addItems(optimizators);
-    right_combo_box->setFixedWidth(200);
-    auto right_part_v_box = new QVBoxLayout(right_part);
-    right_part_v_box->addWidget(right_combo_box);
 }
 
 int main(int argc, char *argv[]) {
