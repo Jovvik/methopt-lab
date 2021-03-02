00a0e9ac97386702283a7062cb59ce6ad6603ab8
Maxim Mikhaylov <mmikhaylov56@gmail.com>
2021-03-02 09:49:59 +0300

    Fixed plotting

--- gui/source/main.cpp
+++ gui/source/main.cpp
@@ -1,26 +1,27 @@
-#include <QPushButton>
-#include <QComboBox>
-#include <QSpinBox>
 #include <QApplication>
-#include <QWidget>
-#include <QIcon>
-#include <QHBoxLayout>
-#include <QVBoxLayout>
-#include <QLineEdit>
-#include <QFile>
-#include <QTextStream>
-#include <iostream>
+#include <QComboBox>
 #include <QDir>
+#include <QFile>
+#include <QHBoxLayout>
+#include <QIcon>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPen>
+#include <QPushButton>
 #include <QSizePolicy>
 #include <QSlider>
-#include <QLabel>
+#include <QSpinBox>
+#include <QTextStream>
+#include <QVBoxLayout>
 #include <QVector>
-#include <QPen>
-#include "qcustomplot.h"
+#include <QWidget>
+#include <iostream>
+
 #include "lab/dichotomy.h"
+#include "qcustomplot.h"
 
 class Drawer : public QWidget {
-public:
+  public:
     Drawer(QWidget *parent = 0) {
         plot_setup();
         auto optimizer = lab::Dichotomy(1e-5);
@@ -29,23 +30,26 @@ public:
     }
 
     void draw(int iteration) {
-        auto[a, b, points] = segments[iteration];
-        std::vector<double> x, y, x_f, y_f;
-        points_to_x_y(points, x, y);
-        points_to_x_y(get_f_points(a, b), x_f, y_f);
+        auto [a, b, _] = segments[iteration];
+        std::vector<double> f_x, f_y;
+        points_to_x_y(get_f_points(-2, 3), f_x, f_y);
         plot->addGraph();
         plot->graph(0)->setPen(QPen(Qt::blue));
-        plot->graph(0)->setData(QVector<double>::fromStdVector(x_f), QVector<double>::fromStdVector(y_f));
+        plot->graph(0)->setData(QVector<double>(f_x.begin(), f_x.end()),
+                                QVector<double>(f_y.begin(), f_y.end()));
         plot->addGraph();
         plot->graph(1)->setPen(QPen(Qt::red));
-        plot->graph(1)->setData(QVector<double>::fromStdVector(x), QVector<double>::fromStdVector(y));
-        plot->graph(1)->rescaleAxes();
+        plot->graph(1)->setData({a, b}, {f(a), f(b)});
+        plot->graph(1)->setScatterStyle(QCPScatterStyle::ssCircle);
+        plot->graph(1)->addData(a, f(a));
+        plot->graph(1)->addData(b, f(b));
+        plot->rescaleAxes();
     }
 
-private:
+  private:
     QCustomPlot *plot;
 
-    void plot_setup(){
+    void plot_setup() {
         plot = new QCustomPlot(this);
         auto grid = new QGridLayout(this);
         grid->addWidget(plot);
@@ -53,12 +57,11 @@ private:
         plot->setInteraction(QCP::iRangeZoom, true);
     }
 
-    static double f(double x) {
-        return x * x;
-    }
+    static double f(double x) { return x * x; }
 
-    std::vector <std::pair<double, double>> get_f_points(double a, double b, int count = 30) {
-        std::vector <std::pair<double, double>> points;
+    std::vector<std::pair<double, double>> get_f_points(double a, double b,
+                                                        int count = 1000) {
+        std::vector<std::pair<double, double>> points;
         double step = (b - a) / count;
         for (double x = a; x < b; x += step) {
             points.emplace_back(x, f(x));
@@ -66,25 +69,25 @@ private:
         return points;
     }
 
-    void points_to_x_y(std::vector <std::pair<double, double>> points, std::vector<double> &x, std::vector<double> &y) {
-        for (auto point:points) {
+    void points_to_x_y(std::vector<std::pair<double, double>> points,
+                       std::vector<double> &x, std::vector<double> &y) {
+        for (auto point : points) {
             x.push_back(point.first);
             y.push_back(point.second);
         }
     }
 
-    std::vector <lab::Segment> segments;
+    std::vector<lab::Segment> segments;
 };
 
 class MainWindow : public QWidget {
-public:
+  public:
     MainWindow(QWidget *parent = 0);
 };
 
 double f(double x) { return x * x; }
 
 MainWindow::MainWindow(QWidget *parent) : QWidget(parent) {
-
     auto v_box = new QVBoxLayout(this);
     auto h_box = new QHBoxLayout(this);
 
@@ -101,17 +104,16 @@ MainWindow::MainWindow(QWidget *parent) : QWidget(parent) {
 
     graphic->draw(10);
     graphic->setStyleSheet("background: yellow");
-    QStringList optimizators = {"Дихотомия", "Золотое сечение", "Фиббоначи", "Параболы", "Брент"};
+    QStringList optimizators
+        = {"Дихотомия", "Золотое сечение", "Фиббоначи", "Параболы", "Брент"};
     combo_box->addItems(optimizators);
     combo_box->setFixedWidth(200);
     slider->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
     connect(slider, &QSlider::valueChanged, label,
             static_cast<void (QLabel::*)(int)>(&QLabel::setNum));
-
 }
 
 int main(int argc, char *argv[]) {
-
     QApplication app(argc, argv);
 
     QFile styles("build/assets/styles.css");
