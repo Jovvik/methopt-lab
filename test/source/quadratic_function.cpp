3159e9e7cec294ec549dc3d26c5014e230b82f1c
Maxim Mikhaylov <mmikhaylov56@gmail.com>
2021-04-10 22:45:08 +0300

    added dim test

--- test/source/quadratic_function.cpp
+++ test/source/quadratic_function.cpp
@@ -7,6 +7,7 @@
 TEST_CASE("qfun") {
     auto f = lab::QuadraticFunction(lab::Matrix({{1, 0.3}, {0.4, 1.8}}),
                                     lab::Vector({1.4, -0.6}), -1.2);
+    CHECK_EQ(f.get_dim(), 2);
     CHECK_EQ(f.get_call_count(), 0);
     CHECK_EQ(f.get_grad_count(), 0);
     CHECK_EQ(f(lab::Vector({1, 2})), doctest::Approx(3.8));
