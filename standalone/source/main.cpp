e6e191b3319de22945be2e7b760815e02f92d30c
Maxim Mikhaylov <mmikhaylov56@gmail.com>
2021-04-14 01:39:09 +0300

    Revert "Forgot an include"
    This reverts commit dc29805f7616dd08815731ae722c0366cec14bec.

--- standalone/source/main.cpp
+++ standalone/source/main.cpp
@@ -2,15 +2,6 @@
 #include <iostream>
 #include <optional>
 
-#include "lab1/brent.h"
 #include "lab1/optimizer.h"
 
-int main() {
-    std::function<std::unique_ptr<lab1::Optimizer>(
-        const std::function<double(double)>&, double, double, double)>
-        f = [](const std::function<double(double)>& optimized_function,
-               double epsilon, double start, double end) {
-            return std::make_unique<lab1::Brent>(optimized_function, epsilon,
-                                                 start, end);
-        };
-}
+int main() {}
