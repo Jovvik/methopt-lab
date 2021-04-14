be8ea9604cb7dcdfb529fd1ae231edeaba0af8d7
Maxim Mikhaylov <mmikhaylov56@gmail.com>
2021-04-14 17:12:47 +0300

    Made quadratic function fields public

--- include/lab2/quadratic_function.h
+++ include/lab2/quadratic_function.h
@@ -10,15 +10,14 @@ namespace lab2 {
      * <> обозначает векторное произведение
      */
     class QuadraticFunction : public NFunction {
-      private:
-        const Matrix A;
-        const Vector b;
-        const double c;
-
       public:
         QuadraticFunction(Matrix&& A, Vector&& b, double c);
         double operator()(Vector x) override;
         Vector grad(Vector x) override;
         std::size_t get_dim() override;
+
+        const Matrix A;
+        const Vector b;
+        const double c;
     };
 }  // namespace lab2
