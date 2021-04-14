2e48ab5bad1dba43a34cdfa0f8d5ee7aca118ac4
Maxim Mikhaylov <mmikhaylov56@gmail.com>
2021-04-14 17:13:47 +0300

    Updated tests

--- test/source/n_optimizer.cpp
+++ test/source/n_optimizer.cpp
@@ -10,6 +10,7 @@
 #include "lab1/optimizer.h"
 #include "lab1/parabola.h"
 #include "lab2/fastest_descent.h"
+#include "lab2/functions.h"
 #include "lab2/gradient_descent.h"
 #include "lab2/matrix.h"
 #include "lab2/quadratic_function.h"
@@ -28,17 +29,12 @@ using namespace lab2;
         });                                                                  \
     _doctest_subcase_idx = 0
 
-std::vector<double> zero_vector{0, 0};  // :(
-QuadraticFunction paraboloid(Matrix({{1, 0}, {0, 1}}), Vector(zero_vector), 1);
-Vector paraboloid_start({1, 1});
-
-QuadraticFunction fun(Matrix({{2, -1}, {-1, 2}}), Vector({4, 3}), 1);
-Vector fun_start({1, 1});
-
+std::vector<double> zero_vector{0, 0};
 std::vector<std::pair<QuadraticFunction, Vector>> fns
-    = {{paraboloid, zero_vector}, {fun, Vector({-11. / 3, -10. / 3})}};
+    = {{Functions::paraboloid(), zero_vector},
+       {Functions::f1(), Vector({-11. / 3, -10. / 3})}};
 
-std::vector<double> epsilons = {1e-1, 1e-2, 1e-3, 1e-4, 1e-5, 1e-6};
+std::vector<double> epsilons = {1, 1e-1, 1e-2, 1e-3, 1e-4, 1e-5, 1e-6};
 
 TEST_CASE("gradient descent") {
     std::vector<double> alphas;
@@ -55,7 +51,7 @@ TEST_CASE("gradient descent") {
 
     for (double a : alphas) {
         for (double epsilon : epsilons) {
-            for (const Vector& starting_point : starting_points) {
+            for (auto& starting_point : starting_points) {
                 for (auto& [f, ans] : fns) {
                     CHECK(
                         (GradientDescent(a).optimize(f, starting_point, epsilon)
@@ -69,6 +65,8 @@ TEST_CASE("gradient descent") {
 }
 
 TEST_CASE("fastest descent") {
+    std::vector<Vector> starting_points = {Vector({1, 1})};
+
     using Gen = std::function<std::unique_ptr<lab1::Optimizer>(
         const std::function<double(double)>&, double, double, double)>;
     Gen generator;
@@ -96,13 +94,6 @@ TEST_CASE("fastest descent") {
            }};
 
     DOCTEST_VALUE_PARAMETERIZED_DATA(generator, generators);
-
-    std::vector<Vector> starting_points;
-    for (double x = 0.5; x <= 4; x *= 2) {
-        for (double y = 0.5; y <= 4; y *= 2) {
-            starting_points.emplace_back(std::vector{x, y});
-        }
-    }
 
     for (double epsilon : epsilons) {
         for (const Vector& starting_point : starting_points) {
