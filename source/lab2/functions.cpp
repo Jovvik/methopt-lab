2ad3bf6f1b2c18bf0a3c9f284a74a2c02690a0e5
mremedios <maryya1314@gmail.com>
2021-04-17 15:05:13 +0300

    img

--- source/lab2/functions.cpp
+++ source/lab2/functions.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include "lab2/functions.h"
 
 #include "lab2/matrix.h"
@@ -10,3 +11,11 @@ QuadraticFunction Functions::paraboloid = QuadraticFunction(
 
 QuadraticFunction Functions::f1
     = QuadraticFunction(new Matrix({{2, -1}, {-1, 2}}, 3), Vector({4, 3}), -1);
+
+QuadraticFunction Functions::f2
+        = QuadraticFunction(new Matrix({{508, 506}, {506, 508}}),
+                            Vector({50, 130}), -111);
+
+QuadraticFunction Functions::f3
+        = QuadraticFunction(new Matrix({{216, 80}, {80, 232}}, 8*(28 + sqrt(101))),
+                            Vector({43, 33}), -211);
