c4953236b67ec621449bca3e71c30b60c49b71e8
Maxim Mikhaylov <mmikhaylov56@gmail.com>
2021-04-17 12:06:51 +0300

    Minor performance improvement in conjugate gradient

--- source/lab2/conjugate_gradient.cpp
+++ source/lab2/conjugate_gradient.cpp
@@ -9,9 +9,10 @@ Vector ConjugateGradient::iteration(QuadraticFunction& f, double) {
         f_grad = f.grad(x);
         p      = f_grad * (-1);
     }
-    double alpha    = std::pow(f_grad.norm(), 2) / (*(f.A) * p * p);
+    Vector t        = *(f.A) * p;
+    double alpha    = std::pow(f_grad.norm(), 2) / (t * p);
     Vector new_x    = x + p * alpha;
-    Vector new_grad = f_grad + (*f.A) * p * alpha;
+    Vector new_grad = f_grad + t * alpha;
     double beta     = std::pow(new_grad.norm(), 2) / std::pow(f_grad.norm(), 2);
     p               = new_grad * (-1) + p * beta;
     f_grad          = new_grad;
