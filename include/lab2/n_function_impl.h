8879439cc41cb94588644df36246edada33dffd4
Maxim Mikhaylov <mmikhaylov56@gmail.com>
2021-06-05 22:21:26 +0300

    Minor refactors

--- include/lab2/n_function_impl.h
+++ include/lab2/n_function_impl.h
@@ -10,15 +10,17 @@
 namespace lab2 {
     class NFunctionImpl : public NFunction {
       public:
-        NFunctionImpl(int dim, const std::function<double(Vector)>& f,
-                      const std::function<Vector(Vector)>& grad_f,
-                      const std::function<Matrix(Vector)>& hessian_f);
+        NFunctionImpl(std::size_t dim, std::function<double(Vector)> f,
+                      std::function<Vector(Vector)> grad_f,
+                      std::function<Matrix(Vector)> hessian_f);
+
         double operator()(Vector x) override;
         Vector grad(Vector x) override;
         std::size_t get_dim() override;
         Matrix hessian(Vector x) override;
 
-        const int dim;
+      private:
+        const std::size_t dim;
         const std::function<double(Vector)> f;
         const std::function<Vector(Vector)> grad_f;
         const std::function<Matrix(Vector)> hessian_f;
--- source/lab2/n_function_impl.cpp
+++ source/lab2/n_function_impl.cpp
@@ -1,18 +1,21 @@
 #include "lab2/n_function_impl.h"
 
+#include <utility>
+
 using namespace lab2;
 
-NFunctionImpl::NFunctionImpl(int dim, const std::function<double(Vector)>& f,
-                             const std::function<Vector(Vector)>& grad_f,
-                             const std::function<Matrix(Vector)>& hessian_f)
-    : dim(dim), f(f), grad_f(grad_f), hessian_f(hessian_f) {}
+NFunctionImpl::NFunctionImpl(std::size_t dim, std::function<double(Vector)> f,
+                             std::function<Vector(Vector)> grad_f,
+                             std::function<Matrix(Vector)> hessian_f)
+    : dim(dim),
+      f(std::move(f)),
+      grad_f(std::move(grad_f)),
+      hessian_f(std::move(hessian_f)) {}
 
-Vector NFunctionImpl::grad(Vector x) { return this->grad_f.operator()(x); }
+Vector NFunctionImpl::grad(Vector x) { return grad_f(x); }
 
-double NFunctionImpl::operator()(Vector x) { return this->f.operator()(x); }
+double NFunctionImpl::operator()(Vector x) { return f(x); }
 
-Matrix NFunctionImpl::hessian(Vector x) {
-    return this->hessian_f.operator()(x);
-}
+Matrix NFunctionImpl::hessian(Vector x) { return hessian_f(x); }
 
 std::size_t NFunctionImpl::get_dim() { return dim; }
